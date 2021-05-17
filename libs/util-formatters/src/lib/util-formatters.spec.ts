760ac95107ea8f0dc8ec7b86c257b37575b8de28
Jonas Zunker <Jonas-Zunker@web.de>
2021-05-17 17:32:46 +0200

    fix rounding test

--- libs/util-formatters/src/lib/util-formatters.spec.ts
+++ libs/util-formatters/src/lib/util-formatters.spec.ts
@@ -2,6 +2,6 @@ import { formatRating } from './util-formatters';
 
 describe('utilFormatters', () => {
   it('should work', () => {
-    expect(formatRating(1.999)).toEqual('1.99');
+    expect(formatRating(1.999)).toEqual('2.00');
   });
 });
