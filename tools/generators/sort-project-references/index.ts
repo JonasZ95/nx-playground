89b14e708ce584f8bb3f2e6314b9a0a75f487fea
Jonas Zunker <Jonas-Zunker@web.de>
2021-05-17 16:56:47 +0200

    lab#14

--- tools/generators/sort-project-references/index.ts
+++ tools/generators/sort-project-references/index.ts
@@ -2,19 +2,29 @@ import { Tree, formatFiles, installPackagesTask, updateJson } from '@nrwl/devkit
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (host: Tree, schema: any) {  
-  updateJson(host, 'workspace.json', (workspace) => ({
-    defaultProject: 'ui',
-    ...workspace
-  }))
+  sortProjects(host, 'workspace.json');
+  sortProjects(host, 'nx.json');
+
+  updateJson(host, 'tsconfig.base.json', (json) => {
+    json.compilerOptions.paths = sortObjectKeys(json.compilerOptions.paths);
+    return json
+  });
+
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
   };
 }
 
-function  sortObjectKeys(obj: any) {
+const sortProjects = (host: Tree, path: string) =>
+  updateJson(host, path, (json) => {
+    json.projects = sortObjectKeys(json.projects);
+    return json
+  });
+
+function sortObjectKeys(obj: any) {
   const sorted = {};
-  for(const key in Object.keys(obj).sort()) {
+  for(const key of Object.keys(obj).sort()) {
     sorted[key] = obj[key];
   }
   return sorted;
